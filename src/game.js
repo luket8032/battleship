065cf7c113637aebec82d3ee587aa81f83476a26
Luke <luket8032@hotmail.com>
2023-08-29 12:06:45 -0500

    Setup for game scripts

--- src/game.js
+++ src/game.js
@@ -1,5 +1,22 @@
 const game = () => {
+    function gameInit() {
+        // code to make both playerboards and place ships
+    }
 
+    function playerTurn() {
+        // code for player to attack computer
+    }
+
+    function computerTurn() {
+        // code for computer to attack player
+    }
+
+    function endGame(board1, board2) {
+        if(board1.checkAllSunk) return 'You won! So epic mode...';
+        if(board2.checkAllSunk) return 'You lost. You stink...'
+    }
+
+    return { gameInit, playerTurn, computerTurn, endGame, }
 }
 
 module.exports = game;
